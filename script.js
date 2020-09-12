082cbc449cbfe4639897a2efac30b10dbbe198c5
jorgeHalo3 <71196148+jorgeHalo3@users.noreply.github.com>
2020-09-12 18:25:47 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+//string
+var firstName = Jorge;
+var lastName = Adorno;
+var age = 23;
+alert("my name is"+ firstName + lastName)
+alert("i am"+ age + "years old")
+if(age < 21){
+    alert("it is illegal for me to drink in the states.")
+}
+else if(age=21){
+    alert("i am just barely able to drink in the states.")
+}
+else if(age>21){
+    alert("i am leggaly able to drink in the states.")
+}
+console.log("we are haveing a ton of fun in class.")
